8231f190c02877e3066cf94b59b6610980e780d2
Mário Mello <Mrio Mello@192.168.25.2>
2014-01-08 10:30:08 -0200

    Correção da área de excluir, individual e  multiplos.

--- public/admin/js/scripts.js
+++ public/admin/js/scripts.js
@@ -98,27 +98,6 @@ $(document).ready(function() {
     }
   });
 
-  $('#excluir_all').change(function() {
-
-    if ($(this).is(':checked')) {
-      $('.excluir').attr('checked', true);
-    } else {
-      $('.excluir').attr('checked', false);
-    }
-
-    /*var checked = $(this).is(':checked');
-     $('.excluir').each(function() {
-     if (checked) {
-     $(this).attr('checked', true);
-     $(this).parent('td').addClass('checked');
-     } else {
-     $(this).attr('checked', false);
-     $(this).parent('td').removeClass('checked');
-     }
-     });*/
-
-  });
-
   var link_prefix = $('#link_prefix').val();
 
   $('.setAtivo').change(function() {
@@ -142,14 +121,27 @@ $(document).ready(function() {
     });
   });
 
+  $("#excluir_all").click(function() {
+    if ($("#excluir_all").is(':checked')) {
+      $('.excluir').prop("checked", true);
+    } else {
+      $('.excluir').prop("checked", false);
+    }
+  });
+
   $('.lixeira_ex').click(function() {
 
-    if ($('.excluir').is(':checked')) {
+    if ($('.excluir').is(':checked') !== true) {
       alert('Não há nenhum ítem selecionado.');
       return;
     }
 
-    var c = confirm('Deseja realmente excluir os ítens selecionados?');
+    var msg = 'Deseja realmente excluir os ítens selecionados?';
+    if ($('.excluir:checked').length === 1) {
+      msg = 'Deseja realmente excluir o item selecionado?';
+    }
+
+    var c = confirm(msg);
 
     if (c) {
       var form = newForm();
@@ -167,10 +159,9 @@ $(document).ready(function() {
   });
 
   $('a.excluir_shortcut').click(function() {
-    $('.excluir').attr('checked', false);
-    $(this).parents('tr').find('.excluir').attr('checked', true);
+    $('.excluir').prop("checked", false);
+    $(this).parents('tr').find('.excluir').prop("checked", true);
     $('.lixeira_ex').click();
-    return;
   });
 
   //_# EVENTO DE DOUBLE CLICK AO CLICAR EM FOTO DE GALERIA -> REMOVER ELEMENTO
