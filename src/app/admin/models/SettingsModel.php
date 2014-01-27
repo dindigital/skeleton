c30330ed6a0b5d91c70080b4a7c2ebfbf042328d
Mário Mello <Mrio Mello@192.168.25.2>
2014-01-27 15:29:09 -0200

    Menu configurações ajustado para ingles

---
+++ src/app/admin/models/SettingsModel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace src\app\admin\models;
+
+use src\app\admin\models\essential\BaseModelAdm;
+use src\app\admin\validators\SettingsValidator as validator;
+
+/**
+ *
+ * @package app.models
+ */
+class SettingsModel extends BaseModelAdm
+{
+
+  public function update ( $id, $info )
+  {
+    $validator = new validator();
+    $validator->setHomeTitle($info['home_title']);
+    $validator->setHomeDescription($info['home_description']);
+    $validator->setHomeKeywords($info['home_keywords']);
+    $validator->setTitle($info['title']);
+    $validator->setDescription($info['description']);
+    $validator->setKeywords($info['keywords']);
+    $validator->throwException();
+
+    $tableHistory = $this->getById($id);
+    $this->_dao->update($validator->getTable(), array('id_settings = ?' => $id));
+    $this->log('U', 'Configurações', $validator->getTable(), $tableHistory);
+  }
+
+}
---
+++ src/app/admin/validators/SettingsValidator.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace src\app\admin\validators;
+
+use src\app\admin\validators\BaseValidator;
+use Din\DataAccessLayer\Table\Table;
+use Respect\Validation\Validator as v;
+use Din\Exception\JsonException;
+
+class SettingsValidator extends BaseValidator
+{
+
+  public function __construct ()
+  {
+    $this->_table = new Table('settings');
+  }
+
+  public function setHomeTitle ( $home_title )
+  {
+
+    if ( !v::string()->notEmpty()->validate($home_title) )
+      JsonException::addException('Title Home é obrigatório');
+
+    $this->_table->home_title = $home_title;
+  }
+
+  public function setHomeDescription ( $home_description )
+  {
+    if ( !v::string()->notEmpty()->validate($home_description) )
+      JsonException::addException('Description Home é obrigatório');
+
+    $this->_table->home_description = $home_description;
+  }
+
+  public function setHomeKeywords ( $home_keywords )
+  {
+    if ( !v::string()->notEmpty()->validate($home_keywords) )
+      JsonException::addException('Keywords Home é obrigatório');
+
+    $this->_table->home_keywords = $home_keywords;
+  }
+
+  public function setTitle ( $title )
+  {
+    if ( !v::string()->notEmpty()->validate($title) )
+      JsonException::addException('Title Internas é obrigatório');
+
+    $this->_table->title = $title;
+  }
+
+  public function setDescription ( $description )
+  {
+    if ( !v::string()->notEmpty()->validate($description) )
+      JsonException::addException('Description Internas é obrigatório');
+
+    $this->_table->description = $description;
+  }
+
+  public function setKeywords ( $keywords )
+  {
+    if ( !v::string()->notEmpty()->validate($keywords) )
+      JsonException::addException('Keywords Internas é obrigatório');
+
+    $this->_table->keywords = $keywords;
+  }
+
+}
