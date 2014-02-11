4f83d1979f8ade8747cf683984407b39f676e8a3
Jair Cueva Junior <junior@dindigital.com>
2014-02-11 14:50:10 -0200

    Ajustes no gitignore + criação de pastas vazias necessarias para o sistema

--- config/config.local.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'url' => 'http://framework.jairjunior.com.br',
-    'smtp_host' => 'smtp.mandrillapp.com',
-    'smtp_user' => 'financeiro@dindigital.com',
-    'smtp_pass' => 'bIWsNmzxJIqaMif5nE2bEg',
-    'bitly' => '326ce4633cba26e1e6b7307f236ab0d91f721479',
-);
