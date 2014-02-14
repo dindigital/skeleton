cf4ab71f459d5618cbfd2a65925f6cb75311174b
Jair Cueva Junior <junior@dindigital.com>
2014-02-14 14:15:49 -0200

    Refatoração AdminPasswordController

--- src/app/admin/controllers/essential/AdminPasswordController.php
+++ src/app/admin/controllers/essential/AdminPasswordController.php
@@ -7,9 +7,6 @@ use src\app\admin\models\essential\AdminPasswordModel as model;
 use Din\Http\Post;
 use Exception;
 use Din\ViewHelpers\JsonViewHelper;
-use Din\Email\Email;
-use Din\Email\SendEmail\SendEmail;
-use Din\Mvc\View\View;
 use Din\Session\Session;
 
 /**
@@ -33,36 +30,11 @@ class AdminPasswordController extends BaseController
 
     try {
       $this->_model->recover_password($email);
-      $this->send_email($email, $this->_model->getToken());
     } catch (Exception $e) {
       JsonViewHelper::display_error_message($e);
     }
 
     JsonViewHelper::display_success_message('E-mail enviado com sucesso, por favor acesse sua conta de e-mail para gerar uma nova senha');
-  }
-
-  private function send_email ( $user_email, $token )
-  {
-
-    $data = array(
-        'link' => URL . '/admin/admin_password/update/' . $token . '/'
-    );
-
-    $email_html = new View;
-    $email_html->addFile('src/app/admin/views/email/recover_password.phtml');
-    $email_html->setData($data);
-
-    $email = new Email;
-    $email->setFrom('suporte@dindigital.com');
-    $email->setTo($user_email);
-    $email->setSubject('Recuperação de Senha - Painel de Controle');
-    $email->setBody($email_html->getResult());
-
-    $sendmail = new SendEmail($email);
-    $sendmail->setHost(SMTP_HOST);
-    $sendmail->setUser(SMTP_USER);
-    $sendmail->setPass(SMTP_PASS);
-    $sendmail->send();
   }
 
   public function get_update ()
