02731f26c6853cf882c3db2ebc2401770c7163d5
mariosmello <mariosmello@marios-imac.home>
2014-03-20 07:49:04 -0300

    Config Local

--- config/config.local.php
+++ config/config.local.php
@@ -1,24 +1,6 @@
 <?php
 
 return array(
-    'db_type' => 'mysql',
-    'db_host' => '127.0.0.1',
-    'db_user' => 'root',
-    'db_pass' => 'mysql',
-    'db_schema' => 'framework',
-    'url' => 'http://framework.jairjunior.com',
-    'clientname' => 'FRAMEWORK INC',
-    'agencia_nome' => 'DIN DIGITAL',
-    'imagem_padrao' => '/admin/images/avatar1_50.jpg',
-    'path_image' => 'public/system/images/',
-    'path_assets' => 'public/system/assets/',
-    'path_replace' => 'public',
-    'smtp_host' => 'xxx',
-    'smtp_user' => 'xxx@xxx.com',
-    'smtp_pass' => 'xxx',
-    'bitly' => 'xxx',
-    'tw_consumer_key' => 'xxx',
-    'tw_consumer_secret' => 'xxx',
-    'tw_access_token' => 'xxx',
-    'tw_access_secret' => 'xxx',
+    'db_pass' => '123',
+    'url' => 'http://framework.dev',
 );
