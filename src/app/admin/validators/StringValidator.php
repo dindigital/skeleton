2cc4dedd4f966bca52ea73907ead6770bede894a
Jair Cueva Junior <junior@dindigital.com>
2014-03-21 15:36:32 -0300

    Remoção dos Validators antigos (Adicionado em forma de comentário so métodos que a StringValidadtor não cobre da antiga)

--- src/app/admin/validators/StringValidator.php
+++ src/app/admin/validators/StringValidator.php
@@ -42,4 +42,40 @@ class StringValidator extends BaseValidator2
       return JsonException::addException("Os campos de {$label} devem conter o mesmo valor");
   }
 
+  /*
+    public function setMinMaxString ( $prop, $label, $min = 1, $max = null )
+    {
+    $value = $this->getValue($prop);
+
+    $message = "O campo {$label} precisa ter no mínimo {$min} caracteres";
+    if ( !is_null($max) )
+    $message .= " e no máximo {$max} caracteres";
+
+    if ( !v::string()->length($min, $max)->validate($value) )
+    return JsonException::addException($message);
+
+    $this->_table->{$prop} = $value;
+    }
+
+    public function setLenghtString ( $prop, $label, $lenght = 1 )
+    {
+    $value = $this->getValue($prop);
+
+    if ( !v::string()->length($lenght, $lenght)->validate($value) )
+    return JsonException::addException("O campo {$label} precisa ter {$lenght} caracteres");
+
+    $this->_table->{$prop} = $value;
+    }
+    public function setRequiredMoney ( $prop, $label )
+    {
+    $value = $this->getValue($prop);
+    $value = Money::filter_sql($value);
+
+    if ( !v::numeric()->validate($value) )
+    return JsonException::addException("O campo {$label} é de preenchimento obrigatório");
+
+    $this->_table->{$prop} = $value;
+    }
+
+   */
 }
