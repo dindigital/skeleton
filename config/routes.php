c2e767dce2ece584144ca899e36453ec5e1d52cf
mariosmello <contato@mariosmello.com>
2014-03-28 11:01:25 -0300

    Adicionando modelo do site ao padrao de routes

--- config/routes.php
+++ config/routes.php
@@ -5,6 +5,10 @@ use Din\Router\RouteTypes;
 return array(
     '' => array(
         '' => array(
+            'controller' => 'site\controllers\IndexController',
+            'method' => 'index',
+        ),
+        '404' => array(
             'controller' => 'admin\controllers\essential\Error404Controller',
             'method' => 'display',
             'type' => RouteTypes::ERR_404,
