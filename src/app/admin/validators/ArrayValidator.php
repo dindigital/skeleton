acf51c7cb63306ce9a8ba62378097ce8bac4da7e
mariosmello <contato@mariosmello.com>
2014-03-31 06:56:01 -0300

    Validações de in_array e key_exists

--- src/app/admin/validators/ArrayValidator.php
+++ src/app/admin/validators/ArrayValidator.php
@@ -15,4 +15,20 @@ class ArrayValidator extends BaseValidator
       return JsonException::addException("É necessáio pelo menos 1 {$label}");
   }
 
+  public function validateArrayExists ( $prop, $array )
+  {
+    $value = $this->getValue($prop);
+
+    if (!in_array($value, $array) )
+      return JsonException::addException("Item {$value} não encontrado no array de opções");
+  }
+
+  public function validateArrayKeyExists ( $prop, $array )
+  {
+    $value = $this->getValue($prop);
+
+    if (!array_key_exists($value, $array) )
+      return JsonException::addException("Item {$value} não encontrado nas chaves do array de opções");
+  }
+
 }
