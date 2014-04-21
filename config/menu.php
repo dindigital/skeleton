3a9d93ee8e63f654a8d2229382cea2a2a0ec0f50
mariosmello <contato@mariosmello.com>
2014-04-21 09:21:11 -0300

    Correção de link do menu para páginas "list"

--- config/menu.php
+++ config/menu.php
@@ -7,11 +7,9 @@ return array(
     ),
     'Lixeira' => array(
         'tbl' => 'trash',
-        'index' => 'list',
     ),
     'Log' => array(
         'tbl' => 'log',
-        'index' => 'list',
     ),
     'Configurações' => array(
         'tbl' => 'settings',
@@ -23,21 +21,17 @@ return array(
     ),
     'Usuários' => array(
         'tbl' => 'admin',
-        'index' => 'list',
     ),
     'Notícias' => array(
         'submenu' => array(
             'Categorias' => array(
                 'tbl' => 'news_cat',
-                'index' => 'list',
             ),
             'Subcategorias' => array(
                 'tbl' => 'news_sub',
-                'index' => 'list',
             ),
             'Notícias' => array(
                 'tbl' => 'news',
-                'index' => 'list',
             ),
         )
     ),
@@ -45,56 +39,44 @@ return array(
         'submenu' => array(
             'Menu' => array(
                 'tbl' => 'page_cat',
-                'index' => 'list',
             ),
             'Página' => array(
                 'tbl' => 'page',
-                'index' => 'list',
             ),
         )
     ),
     'Galeria de Fotos' => array(
         'tbl' => 'photo',
-        'index' => 'list',
     ),
     'Galeria de Vídeos' => array(
         'tbl' => 'video',
-        'index' => 'list',
     ),
     'Galeria de Audio' => array(
         'tbl' => 'audio',
-        'index' => 'list',
     ),
     'Publicações' => array(
         'tbl' => 'publication',
-        'index' => 'list',
     ),
     'Tags' => array(
         'tbl' => 'tag',
-        'index' => 'list',
     ),
     'Pesquisa de Satisfação' => array(
         'tbl' => 'survey',
-        'index' => 'list',
     ),
     'Enquetes' => array(
         'tbl' => 'poll',
-        'index' => 'list',
     ),
     'E-mails' => array(
         'submenu' => array(
             'Grupos' => array(
                 'tbl' => 'mailing_group',
-                'index' => 'list',
             ),
             'E-mails' => array(
                 'tbl' => 'mailing',
-                'index' => 'list'
             ),
         )
     ),
     'Clientes' => array(
         'tbl' => 'customer',
-        'index' => 'list',
     ),
 );
