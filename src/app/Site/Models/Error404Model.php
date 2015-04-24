9fcfe7ecf3ee0a3f76fef1f59015ece4f833a62a
Rafael Paulino <rafaclasses@gmail.com>
2015-04-24 15:30:18 -0300

    Alterações no controlador de erro 404, retirando o código de redirect que existia na cut e nos demais jobs não

--- src/app/Site/Models/Error404Model.php
+++ src/app/Site/Models/Error404Model.php
@@ -2,52 +2,33 @@
 
 namespace Site\Models;
 
-use Site\Helpers\Metatags;
-use Din\Http\Header;
-use Site\Models\DataAccess;
+use Site\Helpers\Metatags\Metatags;
 
 class Error404Model extends BaseModelSite
 {
 
-  public function getPage ()
-  {
+    public function getPage ()
+    {
 
-    $this->setSettings();
-    $this->setNav();
-    $this->setMetatag();
-    $this->setUpperBanner();
-    $this->checkMigration();
+        $this->setSettings();
+        $this->setMetatag();
 
-    return $this->_return;
+        return $this->_return;
 
-  }
-
-  private function setMetatag ()
-  {
-
-    $page_metatags = new Metatags\CreateMetatag();
-    $page_metatags->setTitle('Erro 404');
-    $page_metatags->setDescription('');
-    $page_metatags->setKeywords('');
-
-    $metatags = new Metatags\Metatags($page_metatags);
-
-    $this->_return['metatags'] = $metatags;
-
-  }
-
-  private function checkMigration ()
-  {
-    $uri = Header::getUri();
-
-    $finder = new DataAccess\Find\Migration\Redirect;
-    $finder->setUri($uri);
-    $uri_new = $finder->findUri();
-
-    if ( !is_null($uri_new) ) {
-      Header::redirect($uri_new);
     }
 
-  }
+    private function setMetatag ()
+    {
+
+        $page_metatags = new Metatags\CreateMetatag();
+        $page_metatags->setTitle('Erro 404');
+        $page_metatags->setDescription('');
+        $page_metatags->setKeywords('');
+
+        $metatags = new Metatags\Metatags($page_metatags);
+
+        $this->_return['metatags'] = $metatags;
+
+    }
 
 }
