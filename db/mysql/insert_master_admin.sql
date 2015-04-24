11e8cbdf0121a309fd21114ba16806333dfa36c1
Mário Mello <mario@dindigital.com>
2015-04-24 10:08:55 -0300

    Atualização de Respect no Composer, no uso do respect/config e arquivo de banco de dados

--- db/mysql/insert_master_admin.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `admin` (`id_admin`, `is_active`, `name`, `email`, `password`, `avatar`, `inc_date`, `password_change_date`, `permission`) VALUES
-	('b9211c2ba990c3efab12df5e71e4a359', 1, 'Super Administrador', 'suporte@dindigital.com', '879138abe0bb3979bbb7aa7d861faa86', '/system/uploads/admin/1/avatar/dindigital-logotipo-jpg.jpg', '2014-01-24 17:47:22', NULL, NULL);
--- db/mysql/insert_settings.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `settings` (`id_settings`) VALUES
-	('1');
-
-INSERT INTO `socialmedia_credentials` (`id_socialmedia_credentials`) VALUES
-	('1');
